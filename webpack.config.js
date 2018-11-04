ed434b521a89a460677775e5762bacde3ccb4a0a
Shumai <shumai9@github.com>
2018-11-04 22:36:52 +0000

    installed webpack to help bundle js

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+const moduleObj = {
+  loader: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loaders: ["babel-loader"],
+    }
+  ],
+};
+
+
+
+const client = {
+  entry: {
+    'client' : './src/client/index.js'
+  },
+  target: 'web',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist/public')
+  },
+  module: moduleObj
+};
+
+
+const server = {
+  entry: {
+    'server' : './src/server/index.js',
+    target: 'node',
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist')
+    }
+  },
+  module: moduleObj
+};
+
+module.exports = [client, server];
